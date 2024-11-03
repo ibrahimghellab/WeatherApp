edeab1837339594f36bce993852f5ed1254f866d
ibrahimghellab <ghellabibrahim@gmail.com>
2024-11-03 14:15:59 +0100

    first features

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
 let getWeather=document.getElementById("getWeather");
+let dateP=document.getElementById("date");
+let emojiP=document.getElementById("emoji");
+let temperatureP=document.getElementById("temperature");
+let feelLike=document.getElementById("feelLike");
+let humidity=document.getElementById("humidity");
+let source=document.getElementById("src");
 
 
 
-let getAll=async(city)=>{
-    let request="https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/city?key=F35BBHUQQFCZ2EDBV78NKVA5Q";
+const fahrenheitToCelsius=(degre)=>{
+    return Math.round((degre-32)*5/9);
+}
+
+const getAll=async(city)=>{
+    let request="https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/48.867,2.33?key=F35BBHUQQFCZ2EDBV78NKVA5Q";
     let data=await fetch(request);
     let response= await data.json();
-    
+    console.log(response);
+    let date = new Date().toLocaleDateString();
+    dateP.textContent=date;
+    emojiP.textContent="❄️";
+    temperatureP.textContent=fahrenheitToCelsius(response.currentConditions.temp)+"°C";
+    feelLike.textContent=fahrenheitToCelsius(response.currentConditions.feelslike)+"°C";
+    humidity.textContent=response.currentConditions.humidity+"%";
+    source.textContent="Source : "+response.currentConditions.source;
 }
 
 
-
-getWeather.addEventListener("click",getAll(city));
-
-
+getWeather.addEventListener("click",getAll("Paris,FR"));
 
 
 
